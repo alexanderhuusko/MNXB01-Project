02d1da0e2f2caf5094711ca746a2e778eccb5dc7
Alexander Huusko <kem13ahu@student.lu.se>
2017-11-15 21:49:15 +0100

    adfgladlfglgadf tired of errors from pull requests

--- code/project.cpp
+++ code/project.cpp
@@ -201,27 +201,27 @@ void project(int choice) {
 
 	string dataPathLulea = "../datasets/smhi-opendata_Lulea.csv"; 
 	int startingLineLulea = 11;
-	/*string dataPathKarlstad = "../datasets/smhi-opendata_Karlstad.csv";
+	string dataPathKarlstad = "../datasets/smhi-opendata_Karlstad.csv";
 	int startingLineKarlstad = 12;
 	string dataPathBoras = "../datasets/smhi-opendata_Boras.csv";
 	int startingLineBoras = 11;
 	string dataPathFalsterbo = "../datasets/smhi-opendata_Falsterbo.csv";
 	int startingLineFalsterbo = 11;		
 	string dataPathFalun = "../datasets/smhi-opendata_Falun.csv";
-	int startingLineFalun = 13;	*/			
+	int startingLineFalun = 13;	
 	string dataPathLund = "../datasets/smhi-opendata_Lund.csv";
 	int startingLineLund = 13;
-	/*string dataPathSoderarm = "../datasets/smhi-opendata_Soderarm.csv";
+	string dataPathSoderarm = "../datasets/smhi-opendata_Soderarm.csv";
 	int startingLineSoderarm = 12;
 	string dataPathUmea = "../datasets/smhi-opendata_Umea.csv";
 	int startingLineUmea = 13;
 	string dataPathVisby = "../datasets/smhi-opendata_Visby.csv"; 
-	int startingLineVisby = 11; */
+	int startingLineVisby = 11; 
 	
 	tempTrender lulea(dataPathLulea, startingLineLulea);
 	lulea.readDataFile();
 	
-	/*tempTrender karlstad(dataPathKarlstad, startingLineKarlstad);
+	tempTrender karlstad(dataPathKarlstad, startingLineKarlstad);
 	karlstad.readDataFile();
 
 	tempTrender boras(dataPathBoras, startingLineBoras);
@@ -231,19 +231,19 @@ void project(int choice) {
 	falsterbo.readDataFile();
 			
 	tempTrender falun(dataPathFalun, startingLineFalun);
-	falun.readDataFile();*/
+	falun.readDataFile();
 	
 	tempTrender lund(dataPathLund, startingLineLund);
 	lund.readDataFile();
 	
-	/*tempTrender soderarm(dataPathSoderarm, startingLineSoderarm);
+	tempTrender soderarm(dataPathSoderarm, startingLineSoderarm);
 	soderarm.readDataFile();
 		
 	tempTrender umea(dataPathUmea, startingLineUmea);
 	umea.readDataFile();
 	
 	tempTrender visby(dataPathVisby, startingLineVisby);
-	visby.readDataFile(); */
+	visby.readDataFile(); 
 	
 	switch(choice){
 		case 1 :
